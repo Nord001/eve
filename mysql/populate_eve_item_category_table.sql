b4ca944d1b81602e6d55f72be8d910764aabb3cb
Andy Lo <andy.lo@gmx.com>
2011-06-25 19:32:47 -0500

    Added MySQL Workbench model and scripts for initializing the database.

---
+++ mysql/populate_eve_item_category_table.sql
@@ -0,0 +1,10 @@
+ALTER TABLE item_category AUTO_INCREMENT = 1;
+
+INSERT INTO item_category (`category_name`) VALUES
+    (1,'Ores & Minerals'),
+    (2,'Gas Clouds & Boosters'),
+    (3,'Ice & Ice Products'),
+    (4,'Moon Materials'),
+    (5,'Fullerenes & Polymers'),
+    (6,'Salvage'),
+    (7,'Skills');
