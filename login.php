00d6b08e7dd54ce3fea0a18f02591809ca9f9b8e
Andy Lo <andy.lo@gmx.com>
2011-07-23 20:06:51 -0500

    Modified the login script to use PDO instead of MySQLi.

--- login.php
+++ login.php
@@ -3,65 +3,64 @@
  * Written By: Andy Lo
  */
 
-// Contains functions to sanitize input.
-require_once('sanitize.php');
+require_once('Singleton.php');
 session_start();
 
 /* Checks whether or not the user was directed from the correct page.
  * Also checks whether the username field is empty.
  */
 if(isset($_POST['username'], $_POST['password']) && !empty($_POST['username'])) {
-    // Sets up connection to database.
-    $mysqli = new mysqli('localhost', 'eve', 'jBymeV3UYEZJu4wE', 'eve');
-
-    if(mysqli_connect_errno()) {
-        echo 'Connection failed: ' . mysqli_connect_error();
-        exit();
-    }
-
-    /* Sanitizes form input and initializes session data.  Unnecessary with the
-     * prepared statements, but I left the code in.
-     */
-    $c_username = $mysqli->real_escape_string($_POST['username']);
-    $c_password = $mysqli->real_escape_string($_POST['password']);
     $_SESSION['authenticated'] = false;
+    
+    // Configure database connection.
+    $t_db_dsn = 'mysql:host=localhost;dbname=eve';
+    $t_db_username = 'eve';
+    $t_db_password = 'B968AHTjZZK7NVDx';
 
+    // Gets POST data.
+    $t_username = $_POST['username'];
+    $t_password = $_POST['password'];
+    
     // Cleans POST data.
     unset($_POST['username'], $_POST['password']);
     
+    // Sets up connection to database.
+    try {
+        $db = new PDO($t_db_dsn, $t_db_username, $t_db_password);
+    } catch(PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+    
+    // Set up authentication query.
     $eve_user_table = 'eve_user_table';
+    $query = "SELECT username, password FROM $eve_user_table WHERE username =
+        :username";
     
-    // Prepared statement for the authentication query.
-    $stmt  = $mysqli->stmt_init();
-    $query = "SELECT username, password FROM $eve_user_table WHERE username = ?";
-    
-    if(!$stmt->prepare($query)) {
+    if(!$stmt = $db->prepare($query)) {
         echo 'Failed to prepare statement.';
         exit();
     } else {
-        // Binds the data to the query.
-        $stmt->bind_param('s', $c_username);
+        $stmt->bindParam(':username', $t_username);
 
         // Executes the query and buffers the results.
-        if(!$stmt->execute()) {
-            echo 'Query failed.';
-        } else {
-            $stmt->bind_result($result['username'], $result['password']);
-        
-            // Parses the query results.
-            while($stmt->fetch()) {
-                var_dump($result);
-                /* Authenticates the session data against the results from the
-                 * database.
-                 */
-                if(!strcmp($result['username'], $c_username)
-                    && !strcmp($result['password'], md5($c_password))) {
-                    $_SESSION['authenticated'] = true;
-                    header("Location: grid_edit.php");
-                } else {
-                    echo "Authentication failed."; // Strictly for debugging.
-                    //header("Location: logout.php");
-                }
+        try {
+            $stmt->execute();
+        } catch(PDOException $e) {
+            echo 'Query failed: ' . $e->getMessage();
+        }
+
+        // Parses the query results.
+        while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            /* Authenticates the session data against the results from the
+             * database.
+             */
+            if(!strcmp($result['username'], $t_username)
+                && !strcmp($result['password'], md5($t_password))) {
+                $_SESSION['authenticated'] = true;
+                header("Location: grid_edit.php");
+            } else {
+                echo "Authentication failed."; // Strictly for debugging.
+                //header("Location: logout.php");
             }
         }
     }
